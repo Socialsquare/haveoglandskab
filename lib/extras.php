e741f245fabacc4eb7b46d523a42bf4e37c52e2c
Andreas Larsen <andreas@larsenwork.com>
2016-12-12 13:07:51 +0100

    Shortcode cleanup

--- lib/extras.php
+++ lib/extras.php
@@ -69,25 +69,25 @@ add_filter( 'get_the_archive_title', __NAMESPACE__ . '\\my_theme_archive_title' 
 // Allow shortcodes in widgets
 add_filter('widget_text', 'do_shortcode');
 
-// Button shortcodes
+// Button
+add_shortcode( 'button', __NAMESPACE__ . '\\button' );
 function button( $atts, $content = null ){
   return "<div class='btn btn-white btn-lg m-r-1 m-b-1'>" . $content . "</div>";
-
 }
-add_shortcode( 'button', __NAMESPACE__ . '\\button' );
 
-// Column shortcodes
+// Columns
+add_shortcode( 'column', __NAMESPACE__ . '\\column' );
 function column( $atts, $content = null ) {
 	return "<div class='col-sm-6'>" . $content . "</div>";
 }
-add_shortcode( 'column', __NAMESPACE__ . '\\column' );
 
+add_shortcode( 'column-wide', __NAMESPACE__ . '\\columnWide' );
 function columnWide( $atts, $content = null ) {
 	return "<div class='col-xs-12'>" . $content . "</div>";
 }
-add_shortcode( 'column-wide', __NAMESPACE__ . '\\columnWide' );
 
 // Social Icons
+add_shortcode( 'icon', __NAMESPACE__ . '\\icon' );
 function icon( $atts ) {
   $a = shortcode_atts( array(
     'logo' => '',
@@ -95,9 +95,9 @@ function icon( $atts ) {
   ), $atts );
 	return "<a href='{$a['url']}' target='_blank' class='social-icon'><img class='social-icon__img' src='". get_template_directory_uri() . "/dist/images/{$a['logo']}.svg'/></a>";
 }
-add_shortcode( 'icon', __NAMESPACE__ . '\\icon' );
 
 // Logo
+add_shortcode( 'logo', __NAMESPACE__ . '\\logo' );
 function logo( $atts ) {
   $a = shortcode_atts( array(
     'logo' => '',
@@ -105,4 +105,3 @@ function logo( $atts ) {
   ), $atts );
 	return "<a href='{$a['url']}' target='_blank' class='social-icon'><img class='social-icon__img-big m-r-1' src='". get_template_directory_uri() . "/dist/images/{$a['logo']}.svg'/></a>";
 }
-add_shortcode( 'logo', __NAMESPACE__ . '\\logo' );
