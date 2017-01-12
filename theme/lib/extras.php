da4e9efcbaf2374eace7236057a6c1730d81e392
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-01-12 10:19:45 +0100

    Adding the white button as a textstyle

--- theme/lib/extras.php
+++ theme/lib/extras.php
@@ -151,3 +151,27 @@ function my_map_meta_cap( $caps, $cap, $user_id, $args ) {
 	/* Return the capabilities required by the user. */
 	return $caps;
 }
+
+// Attach callback to 'tiny_mce_before_init'
+// @see https://codex.wordpress.org/Plugin_API/Filter_Reference/tiny_mce_before_init
+add_filter( 'tiny_mce_before_init', function( $init_array ) {
+  // Define the style_formats array
+  $style_formats = array(
+    // Each array child is a format with it's own settings
+    array(
+      'title' => __('White button', 'haveoglandskab'),
+      'selector' => 'a',
+      'classes' => array('btn', 'btn-lg', 'btn-white')
+    )
+  );
+  // Insert the array, JSON ENCODED, into 'style_formats'
+  $init_array['style_formats'] = json_encode( $style_formats );
+
+  return $init_array;
+});
+
+add_filter( 'mce_buttons_2', function( $buttons ) {
+  //Add style selector to the beginning of the toolbar
+  array_unshift( $buttons, 'styleselect' );
+  return $buttons;
+ });
