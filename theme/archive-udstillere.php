c08074e6f1e6a0d4083f2c1d6df1d6351748110a
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-01-17 03:01:29 +0100

    "Filtrér efter branche" instead of "Kategorier", 2x spaces and BEM styling

--- theme/archive-udstillere.php
+++ theme/archive-udstillere.php
@@ -1,16 +1,16 @@
 <?php // get_search_form(); ?>
 
-<div class='filter-bar'>
-  <h2> Kategorier:  </h2>
+<div class="filter-bar">
+  <h2>Filtrér efter branche</h2>
   <?php
-    $terms = get_terms( array('taxonomy' => 'sector') );
-  
-    foreach ($terms as $term) {
-      echo '<a class="btn btn-white-flat" href="/branche/' . $term->slug . '">' .$term->name. '</a>';
-    }
+  $terms = get_terms( array('taxonomy' => 'sector') );
+  foreach ($terms as $term) {
+    echo '<a class="btn btn-white-flat filter-bar__btn" href="/branche/' . $term->slug . '">' .$term->name. '</a>';
+  }
   ?>
 </div>
 
+<h2>Alle udstillere</h2>
 <?php
   echo '<ul class="udstiller__list row">';
   while (have_posts()) : the_post();
