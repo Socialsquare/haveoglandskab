b1e2710d4d52428e39420255d5c3ffd8f676be38
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-01-20 14:18:45 +0100

    Changed the labels for the custom taxonomies

--- theme/lib/taxonomies.php
+++ theme/lib/taxonomies.php
@@ -6,7 +6,18 @@ function sector_init() {
 		'udstillere',
 		array(
 			'hierarchical' => true,
-			'label' => __( 'Branche' ),
+			'labels' => array(
+				'add_new_item' => __( 'Tilføj ny branche' ),
+				'add_new' => __( 'Tilføj ny' ),
+				'all_items' => __( 'Alle brancher' ),
+				'edit_item' => __( 'Rediger branche' ),
+				'name' => __( 'Brancher' ),
+				'new_item' => __( 'Ny branche' ),
+				'not_found' => __( 'Ingen branche fundet' ),
+				'search_items' => __( 'Søg i brancher' ),
+				'singular_name' => __( 'Branche' ),
+				'view_item' => __( 'Se branche' ),
+			),
 			'rewrite' => array(
 				'slug' => 'branche',
 				'hierarchical' => true, ),
@@ -25,7 +36,18 @@ function area_init() {
 		'udstillere',
 		array(
 			'hierarchical' => true,
-			'label' => __( 'Område' ),
+			'labels' => array(
+				'add_new_item' => __( 'Tilføj nyt område' ),
+				'add_new' => __( 'Tilføj ny' ),
+				'all_items' => __( 'Alle områder' ),
+				'edit_item' => __( 'Rediger område' ),
+				'name' => __( 'Områder' ),
+				'new_item' => __( 'Nyt område' ),
+				'not_found' => __( 'Ingen områder fundet' ),
+				'search_items' => __( 'Søg i områder' ),
+				'singular_name' => __( 'Område' ),
+				'view_item' => __( 'Se område' ),
+			),
 			'meta_box_cb' => false,
 			'rewrite' => array(
 				'slug' => 'area',
