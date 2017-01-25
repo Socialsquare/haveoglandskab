811425c229059b12e1ddfb9bdde03b0f6790eb89
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-01-25 13:27:51 +0100

    Simplified the titles on area and sector taxonomy pages

--- theme/taxonomy-sector.php
+++ theme/taxonomy-sector.php
@@ -1,4 +1,5 @@
 <?php
+  echo '<h3>Udstillere</h3>';
   echo '<ul class="udstiller__list row">';
   while (have_posts()) : the_post();
     get_template_part('templates/content-udstiller', get_post_type());
